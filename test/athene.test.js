58660c131451e1c13c3d3111ee98467328bdf0ac
Mohammed Neundorf <Mohammed Neundorf>
2015-11-10 13:53:43 +0000

    added body structure to protos, added tests for static methods and prototypes

--- test/athene.test.js
+++ test/athene.test.js
@@ -6,6 +6,41 @@ describe('Athene', function() {
 	});
 
 	it('should be able to instantiate an athene app', function() {
-		var application = new Athene();
+		new Athene("dummyTarget", {});
+	});
+
+	it('should throw an exception if no target defined', function() {
+		(function(){
+		  new Athene();
+		}).should.throw('No target defined');
+	});
+
+	it('should have a static method load', function() {
+		Athene.should.have.property('load');
+		Athene.load.should.be.a.Function;
+	});
+
+	it('should have a static method bootstrap', function() {
+		Athene.should.have.property('bootstrap');
+		Athene.bootstrap.should.be.a.Function;
+	});
+
+	context('instance', function() {
+		var instance = new Athene("dummyTarget", {});
+
+		it('should have a method bootstrap', function() {
+			instance.should.have.property('bootstrap');
+			instance.bootstrap.should.be.a.Function;
+		});
+
+		it('should have a method start', function() {
+			instance.should.have.property('start');
+			instance.start.should.be.a.Function;
+		});
+
+		it('should have a method stop', function() {
+			instance.should.have.property('stop');
+			instance.stop.should.be.a.Function;
+		});
 	});
 });
